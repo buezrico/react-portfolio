7d3020e7cf37cb2bcaddefb31809c07d0caf3dff
buezrico <buezokoronkwo@gmail.com>
2025-07-22 08:40:47 +0000

    fix: change the links on the portfolio section that wasn't working

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -15,7 +15,6 @@ const data = [
     id: 1,
     image: billionbros,
     title: "BILLION BROS",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://billionbros.ng/",
     description:
       "Billionaire Brothers is a thrilling online platform where users can earn daily by playing fun games and joining impactful raffles.",
@@ -24,7 +23,6 @@ const data = [
     id: 2,
     image: thegrandcobbler,
     title: "THE GRAND COBBLER",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://thegrandcobbler.com/",
     description:
       "THE GRAND COBBLER is Lagos' finest and most reliable shoes and leather goods repair company. An on-demand service providing modern aftercare.",
@@ -33,8 +31,7 @@ const data = [
     id: 3,
     image: codeafrica,
     title: "CODE AFRICA",
-    // github: "https://github.com/igudy/shopiverse",
-    demo: "https://codeafrica.vercel.com/",
+    demo: "https://codeafrica.vercel.app/",
     description:
       "Code Africa is a Next.js and Sass-powered platform for digital skills training. Students can register and pay for courses, it also has an admin dashboard.",
   },
@@ -42,8 +39,7 @@ const data = [
     id: 4,
     image: assetplus,
     title: "ASSETPLUS",
-    // github: "https://github.com/igudy/shopiverse",
-    demo: "https://assetplus.vercel.com/",
+    demo: "https://assetplus.vercel.app/",
     description:
       "AssetPlus is a premier platform for buying land properties, offering a seamless and secure purchasing experience with a robust search feature.",
   },
@@ -51,7 +47,6 @@ const data = [
     id: 5,
     image: valucares,
     title: "VALUCARES",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://valucares.com/",
     description:
       "Valucares is an online, real-time healthcare provider. Customers can book appointments for health checks seamlessly.",
@@ -60,7 +55,6 @@ const data = [
     id: 6,
     image: tourafrica,
     title: "TOURAFRICA",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://tourafrica.vercel.app",
     description:
       "TourAfrica is a comprehensive travel platform that offers curated African travel experiences. Explore and book unique tours, accommodations, and activities with ease.",
@@ -69,7 +63,6 @@ const data = [
     id: 7,
     image: insightproxim,
     title: "INSIGHTPROXIMCONSULTING",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://insightproximconsulting.com.ng/",
     description:
       "InsightProxim Consulting provides expert guidance and solutions in business strategy and management. Tailored consulting services to help organizations achieve their goals.",
@@ -78,7 +71,6 @@ const data = [
     id: 8,
     image: synproxi,
     title: "SYNPROXI",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://synproxi.com.ng/",
     description:
       "Synproxi offers free digital skills training to empower individuals with the knowledge and tools to succeed in the digital world.",
@@ -87,7 +79,6 @@ const data = [
     id: 9,
     image: nkechi,
     title: "NKECHI KEN",
-    // github: "https://github.com/igudy/shopiverse",
     demo: "https://nkechikennkwonta.com/",
     description:
       "A Portfolio website for Mrs Nkechi Ken. Built with NextJs, TypeScript and Sass.",
